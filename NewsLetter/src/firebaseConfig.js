1779d0010cbbfe7355941098fd4eb19bff28d7da
kanimozhi <kanimozhi.v@techmango.net>
2020-03-16 14:55:25 +0530

    firebase changes

---
+++ NewsLetter/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrbWxZbdKU28WMgX1arMltdBA8sxW-09w",
+    authDomain: "news-letter-c4cc3.firebaseapp.com",
+    databaseURL: "https://news-letter-c4cc3.firebaseio.com",
+    projectId: "news-letter-c4cc3",
+    storageBucket: "news-letter-c4cc3.appspot.com",
+    messagingSenderId: "811974354757",
+    appId: "1:811974354757:web:c3016a3bed3715f6c3402c",
+    measurementId: "G-9L6MJ76WSJ"
+  };

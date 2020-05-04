95da1cb747522e4a6d6764380a2394d71eba2a6f
kanimozhi <kanimozhi.v@techmango.net>
2020-05-04 16:36:19 +0530

    newsletter changes

--- NewsLetter/src/App.js
+++ NewsLetter/src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import { Router } from "@reach/router";
 import ToolBarCustom from './Components/toolBarCustom';
 import SignUpLayout from './SignUpForm/signUpLayout';
 import LogIn from './Components/loginIn';
 import './App.css';
+import NavBar from './Components/MainLayout/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-       <ToolBarCustom />
-         <Router>
-           <SignUpLayout path='/' />
-           <LogIn path="/login" />
-        </Router>
-    </div>
-  );
+class App extends Component {
+  state = {
+      isLoggedIn: false
+    };
+  handleLogIn = (validation) =>{
+     console.log('this.state.login:::', validation);
+     this.setState({isLoggedIn : validation})
+  }; 
+  render() {
+    return (
+      <div>
+        {this.state.isLoggedIn ? (
+          <div className="App">
+             <NavBar />
+          </div>
+        ) : (
+          <div className="App">
+              <ToolBarCustom />
+              <Router>
+                <SignUpLayout path='/' />
+                <LogIn  handleLogInClick={this.handleLogIn} path="/login" />
+              </Router>
+           </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- NewsLetter/src/Components/loginIn.js
+++ NewsLetter/src/Components/loginIn.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { makeStyles} from "@material-ui/core/styles";
+import React, {Component} from 'react';
+import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import { useFormik } from 'formik';
 import './logIn.css';
 
-const useStyles = makeStyles(({
+const styles = theme =>({
     root: {
       flexGrow: 1,
       flexDirection: "row",
@@ -19,72 +18,62 @@ const useStyles = makeStyles(({
       display: "flex",
       flexDirection: "column",
     }
-}));
+});
 
-export default function LogIn() {
-  const validate = values => {
-    const errors = {};
-    if (!values.userName) {
-      errors.userName = 'Required';
-    }
-  
-    if (!values.password) {
-      errors.password = 'Required';
-    }
-    
-    return errors;
+
+class LogIn extends Component{
+  state = {
+    userName: '',
+    password: ''
   };
-  const formik = useFormik({
-    initialValues: {
-      userName: '',
-      password: ''
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-        <div> 
-          <form className="width-100" onSubmit={formik.handleSubmit}>
-              <div className={classes.child}>
-                  <div className="child-container">
-                      <TextField
-                          id="userName"
-                          name="userName"
-                          type="text"
-                          label="User Name"
-                          className="textField"
-                          onChange={formik.handleChange}
-                          onBlur={formik.handleBlur}
-                          value={formik.values.userName}
-                      />
-                      {formik.touched.userName && formik.errors.userName ? <div>{formik.errors.userName}</div> : null}
-                  </div>
-                  <div className="child-container">
-                      <TextField
-                          id="password"
-                          name="password"
-                          type="password"
-                          label="Password"
-                          className="textField"
-                          onChange={formik.handleChange}
-                          onBlur={formik.handleBlur}
-                          value={formik.values.password}
-                      />
-                      {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
-                  </div>
-                  <div className="child-container">
-                    <Button className="textField"  variant="outlined" color="primary" type="submit">Log In</Button>
-                  </div>
-              </div>
-          </form>
-        </div>
-        <div className={classes.child}>
-           <img className="image-style" src="/images/signupImage.jpg" alt="SignUp" />
-        </div>
-    </div>
-  );
+
+  handleChange = changedValue => event => {
+    this.setState({ [changedValue]: event.target.value });
+  };
+  sendData = () =>{
+    this.props.handleLogInClick(true);
+  }
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+          <div> 
+            <form className="width-100">
+                <div className={classes.child}>
+                    <div className="child-container">
+                        <TextField
+                            id="userName"
+                            name="userName"
+                            type="text"
+                            label="User Name"
+                            className="textField"
+                            onChange={this.handleChange('userName')}
+                            value={this.state.userName}
+                        />
+                    </div>
+                    <div className="child-container">
+                        <TextField
+                            id="password"
+                            name="password"
+                            type="password"
+                            label="Password"
+                            className="textField"
+                            onChange={this.handleChange('password')}
+                            value={this.state.password}
+                        />
+                    </div>
+                    <div className="child-container">
+                      <Button className="textField" onClick={this.sendData} variant="outlined" color="primary">Log In</Button>
+                    </div>
+                </div>
+            </form>
+          </div>
+          <div className={classes.child}>
+             <img className="image-style" src="/images/signupImage.jpg" alt="SignUp" />
+          </div>
+      </div>
+    );
+  }
+ 
 }
+export default withStyles(styles)(LogIn);
